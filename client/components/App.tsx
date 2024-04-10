7b5fce421279c162521f645cbdd81d559d4bc7f7
Ben Atkin <63535606+ben-atkin@users.noreply.github.com>
2024-04-11 11:52:46 +1200

    Revert "Countdown timer"

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,23 +1,13 @@
-import React, { useState } from 'react'
-import Grid from '../components/game-grid'
-import CountdownTimer from '../components/Countdown'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  const gameDuration = 60000
-  const [gameStarted, setGameStarted] = useState(false)
-
-  const startGame = () => {
-    setGameStarted(true)
-  }
-
   return (
     <>
       <div className="app">
         <h1>Aim Trainer</h1>
-        {gameStarted && <CountdownTimer duration={gameDuration} />}
       </div>
       <div className="grid-container">
-        <Grid onStartGame={startGame} />
+        <Outlet />
       </div>
     </>
   )
