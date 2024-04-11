51bdf439ecb75ac77e962c9646fa5fcc0f7f3048
Jess Bayman <154103314+jess-bay@users.noreply.github.com>
2024-04-11 15:29:16 +1200

    Write skeletons for player route server side tests

--- server/routes/__tests__/playerRoutes.test.ts
+++ server/routes/__tests__/playerRoutes.test.ts
@@ -1,22 +1,203 @@
-import { it, expect, describe, vi } from 'vitest'
+import {
+  it,
+  expect,
+  describe,
+  vi,
+  beforeAll,
+  beforeEach,
+  afterAll,
+} from 'vitest'
 import request from 'supertest'
 import * as db from '../../db/functions/playerFunctions.ts'
 import server from '../../server.ts'
+import connection from '../../db/connection.ts'
 
-// describe('GET /api/v1/gamemode ', () => {
-//   it.todo('Should get all game mode data', async () => {
-//     vi.mocked(db.getAllGameModes).mockResolvedValue(dummyDataArr)
+const rootUrl = '/api/v1/player'
 
-//     const res = await request(server).get(`${rootUrl}`)
+beforeAll(() => {
+  return connection.migrate.latest()
+})
 
-//     expect(res.statusCode).toBe(200)
-//     expect(res.body).toStrictEqual(dummyDataArr)
-//   })
-//   it('Should show an error message', async () => {
-//     vi.mocked(db.getAllGameModes).mockRejectedValue(dummyDataArr)
+beforeEach(async () => {
+  await connection.seed.run()
+})
 
-//     const res = await request(server).get(`${rootUrl}`)
+const dummyDataArr = [
+  {
+    id: 1,
+    name: 'Joel',
+    score: 1,
+    time_taken: '',
+    gamemode_id: 1,
+  },
+  {
+    id: 2,
+    name: 'Jess',
+    score: 10,
+    time_taken: '',
+    gamemode_id: 1,
+  },
+  {
+    id: 3,
+    name: 'Boston',
+    score: 20,
+    time_taken: '',
+    gamemode_id: 1,
+  },
+]
 
-//     expect(res.statusCode).toBe(500)
-//   })
-// })
+const dummyData = {
+  id: 1,
+  name: 'Joel',
+  score: 1,
+  time_taken: '',
+  gamemode_id: 1,
+}
+
+describe('GET /api/v1/player ', () => {
+  it('Should get all players', async () => {
+    const res = await request(server).get(`${rootUrl}`)
+
+    expect(res.body).toHaveLength(3)
+    expect(res.body).toStrictEqual(dummyDataArr)
+    expect(res.body).toMatchInlineSnapshot(`
+      [
+        {
+          "gamemode_id": 1,
+          "id": 1,
+          "name": "Joel",
+          "score": 1,
+          "time_taken": "",
+        },
+        {
+          "gamemode_id": 1,
+          "id": 2,
+          "name": "Jess",
+          "score": 10,
+          "time_taken": "",
+        },
+        {
+          "gamemode_id": 1,
+          "id": 3,
+          "name": "Boston",
+          "score": 20,
+          "time_taken": "",
+        },
+      ]
+    `)
+  })
+})
+
+describe('GET by ID /api/v1/player/:id', () => {
+  it('Should get player based on an ID', async () => {
+    const res = await request(server).get(`${rootUrl}/1`)
+
+    expect(res.body).toStrictEqual(dummyData)
+    expect(res.body).toMatchInlineSnapshot(`
+      {
+        "gamemode_id": 1,
+        "id": 1,
+        "name": "Joel",
+        "score": 1,
+        "time_taken": "",
+      }
+    `)
+  })
+})
+
+describe('POST /api/v1/player', () => {
+  it('Should add a player to the database', async () => {
+    const addDummyData = {
+      name: 'NEW DATA',
+      score: 100,
+      time_taken: 'NEW DATA',
+      gamemode_id: 1,
+    }
+    await request(server).post('/api/v1/player').send(addDummyData)
+
+    const res = await request(server).get('/api/v1/player')
+
+    expect(res.body).toHaveLength(4)
+    expect(res.body).toMatchInlineSnapshot(`
+      [
+        {
+          "gamemode_id": 1,
+          "id": 1,
+          "name": "Joel",
+          "score": 1,
+          "time_taken": "",
+        },
+        {
+          "gamemode_id": 1,
+          "id": 2,
+          "name": "Jess",
+          "score": 10,
+          "time_taken": "",
+        },
+        {
+          "gamemode_id": 1,
+          "id": 3,
+          "name": "Boston",
+          "score": 20,
+          "time_taken": "",
+        },
+        {
+          "gamemode_id": null,
+          "id": 4,
+          "name": "NEW DATA",
+          "score": 100,
+          "time_taken": null,
+        },
+      ]
+    `)
+  })
+})
+
+describe('PATCH /api/v1/player/:id', () => {
+  it('Should update a player based on ID', async () => {
+    const patchDummyData = {
+      name: 'PATCHED DATA',
+      score: 100,
+      time_taken: 'NEW DATA',
+      gamemode_id: 1,
+    }
+    await request(server).patch('/api/v1/player/2').send(patchDummyData)
+
+    const res = await request(server).get('/api/v1/player')
+
+    // expect(res.body).toHaveLength(3)
+    expect(res.body).toMatchInlineSnapshot()
+  })
+})
+
+describe('DELETE /api/v1/player/:id', () => {
+  it('Should delete a game mode based on ID', async () => {
+    await request(server).delete('/api/v1/player/2')
+
+    const res = await request(server).get('/api/v1/player')
+
+    expect(res.body).toHaveLength(2)
+    expect(res.body).toMatchInlineSnapshot(`
+      [
+        {
+          "gamemode_id": 1,
+          "id": 1,
+          "name": "Joel",
+          "score": 1,
+          "time_taken": "",
+        },
+        {
+          "gamemode_id": 1,
+          "id": 3,
+          "name": "Boston",
+          "score": 20,
+          "time_taken": "",
+        },
+      ]
+    `)
+  })
+})
+
+afterAll(() => {
+  connection.destroy()
+})
