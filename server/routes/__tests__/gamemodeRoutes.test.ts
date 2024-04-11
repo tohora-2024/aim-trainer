c3d82502e74edc018e4f7d368d1d0cf4e9f55e5b
Jess Bayman <154103314+jess-bay@users.noreply.github.com>
2024-04-11 15:16:03 +1200

    Finish tests for game mode db functions and routes

--- server/routes/__tests__/gamemodeRoutes.test.ts
+++ server/routes/__tests__/gamemodeRoutes.test.ts
@@ -12,8 +12,6 @@ import * as db from '../../db/functions/gamemodeFunctions.ts'
 import server from '../../server.ts'
 import connection from '../../db/connection.ts'
 
-vi.mock('../../db/functions/gamemodeFunctions')
-
 const rootUrl = '/api/v1/gamemode'
 
 beforeAll(() => {
@@ -25,106 +23,142 @@ beforeEach(async () => {
 })
 
 const dummyDataArr = [
-  { id: 1, name: 'Classic 1 Minute', timeLeft: '1:00' },
-  { id: 2, name: 'Classic 2 Minute', timeLeft: '1:00' },
-  { id: 3, name: 'Classic 3 Minute', timeLeft: '1:00' },
+  { id: 1, name: 'Classic 1 Minute', time_left: '1:00' },
+  { id: 2, name: 'Classic 2 Minute', time_left: '1:00' },
+  { id: 3, name: 'Classic 3 Minute', time_left: '1:00' },
 ]
 
-const dummyData = { id: 1, name: 'Classic 1 Minute', timeLeft: '1:00' }
+const dummyData = { id: 1, name: 'Classic 1 Minute', time_left: '1:00' }
 
 describe('GET /api/v1/gamemode ', () => {
   it('Should get all game mode data', async () => {
-    vi.mocked(db.getAllGameModes).mockResolvedValue(dummyDataArr)
-
     const res = await request(server).get(`${rootUrl}`)
 
-    expect(res.statusCode).toBe(200)
+    expect(res.body).toHaveLength(3)
     expect(res.body).toStrictEqual(dummyDataArr)
-  })
-  it('Should show an error message', async () => {
-    vi.mocked(db.getAllGameModes).mockRejectedValue(dummyDataArr)
-
-    const res = await request(server).get(`${rootUrl}`)
-
-    expect(res.statusCode).toBe(500)
+    expect(res.body).toMatchInlineSnapshot(`
+      [
+        {
+          "id": 1,
+          "name": "Classic 1 Minute",
+          "time_left": "1:00",
+        },
+        {
+          "id": 2,
+          "name": "Classic 2 Minute",
+          "time_left": "1:00",
+        },
+        {
+          "id": 3,
+          "name": "Classic 3 Minute",
+          "time_left": "1:00",
+        },
+      ]
+    `)
   })
 })
 
 describe('GET by ID /api/v1/gamemode/:id', () => {
   it('Should get data based on an ID', async () => {
-    // Arrange
-    vi.mocked(db.getGameModeById).mockResolvedValue(dummyData)
-
-    // Act
     const res = await request(server).get(`${rootUrl}/1`)
 
-    // Assert
-    expect(res.statusCode).toBe(200)
-
     expect(res.body).toStrictEqual(dummyData)
-  })
-  it('Should show an error message', async () => {
-    vi.mocked(db.getGameModeById).mockRejectedValue(dummyData)
-
-    // Act
-    const res = await request(server).get('/api/v1/gamemode/1')
-
-    // Assert
-    expect(res.statusCode).toBe(500)
+    expect(res.body).toMatchInlineSnapshot(`
+      {
+        "id": 1,
+        "name": "Classic 1 Minute",
+        "time_left": "1:00",
+      }
+    `)
   })
 })
 
 describe('POST /api/v1/gamemode', () => {
-  it.todo('Should add a gamemode to the database', async () => {
-    // Arrange
-    const dummyDataNew = { name: 'Classic 4 Minute', timeLeft: '2:00' }
-    vi.mocked(db.addGameMode).mockResolvedValue(dummyDataNew)
-    const res = await request(server).post('/api/v1/gamemode')
+  it('Should add a gamemode to the database', async () => {
+    const addDummyData = { name: 'NEW DATA', timeLeft: 'NEW DATA' }
+    await request(server).post('/api/v1/gamemode').send(addDummyData)
 
-    // Act
-    vi.mocked(db.getAllGameModes).mockResolvedValue(dummyDataArr)
-    const res2 = await request(server).get('/api/v1/gamemode')
-
-    // Assert
-    console.log(res2.body, '😒🤣🤣😂❤')
-    expect(res.statusCode).toBe(200)
-    expect(res2.body).toHaveLength(4)
-  })
-  it.todo('Should show an error message', async () => {
-    const dummyDataNew = { name: 'Classic 4 Minute', timeLeft: '2:00' }
-    vi.mocked(db.addGameMode).mockRejectedValue(dummyDataNew)
-
-    // Act
     const res = await request(server).get('/api/v1/gamemode')
 
-    // Assert
-    expect(res.statusCode).toBe(500)
+    expect(res.body).toHaveLength(4)
+    expect(res.body).toMatchInlineSnapshot(`
+      [
+        {
+          "id": 1,
+          "name": "Classic 1 Minute",
+          "time_left": "1:00",
+        },
+        {
+          "id": 2,
+          "name": "Classic 2 Minute",
+          "time_left": "1:00",
+        },
+        {
+          "id": 3,
+          "name": "Classic 3 Minute",
+          "time_left": "1:00",
+        },
+        {
+          "id": 4,
+          "name": "NEW DATA",
+          "time_left": "NEW DATA",
+        },
+      ]
+    `)
   })
 })
-// describe('/secrets', () => {
-//   it('should send mystical secrets when query is mystical', async () => {
-//     //Act
-//     const res = await request(server).get('/secrets?type=mystical')
-
-//     //Assert
-//     expect(res.statusCode).toBe(200)
-//     expect(res.body).toStrictEqual(['so mystical'])
-//   })
-// })
 
 describe('PATCH /api/v1/gamemode/:id', () => {
-  it.todo('Should update a game mode based on ID', async () => {
-    // Arrange
-    // Act
-    // Assert
+  it('Should update a game mode based on ID', async () => {
+    const patchDummyData = { name: 'PATCHED DATA', timeLeft: 'PATCHED DATA' }
+    await request(server).patch('/api/v1/gamemode/2').send(patchDummyData)
+
+    const res = await request(server).get('/api/v1/gamemode')
+
+    expect(res.body).toHaveLength(3)
+    expect(res.body).toMatchInlineSnapshot(`
+      [
+        {
+          "id": 1,
+          "name": "Classic 1 Minute",
+          "time_left": "1:00",
+        },
+        {
+          "id": 2,
+          "name": "PATCHED DATA",
+          "time_left": "PATCHED DATA",
+        },
+        {
+          "id": 3,
+          "name": "Classic 3 Minute",
+          "time_left": "1:00",
+        },
+      ]
+    `)
   })
 })
 
 describe('DELETE /api/v1/gamemode/:id', () => {
-  it.todo('Should delete a game mode based on ID', async () => {
-    // Arrange
-    // Act
-    // Assert
+  it('Should delete a game mode based on ID', async () => {
+    await request(server).delete('/api/v1/gamemode/2')
+
+    const res = await request(server).get('/api/v1/gamemode')
+
+    expect(res.body).toHaveLength(2)
+    expect(res.body).toMatchInlineSnapshot(`
+      [
+        {
+          "id": 1,
+          "name": "Classic 1 Minute",
+          "time_left": "1:00",
+        },
+        {
+          "id": 3,
+          "name": "Classic 3 Minute",
+          "time_left": "1:00",
+        },
+      ]
+    `)
   })
 })
 
--- server/routes/__tests__/playerRoutes.test.ts
+++ server/routes/__tests__/playerRoutes.test.ts
@@ -2,3 +2,21 @@ import { it, expect, describe, vi } from 'vitest'
 import request from 'supertest'
 import * as db from '../../db/functions/playerFunctions.ts'
 import server from '../../server.ts'
+
+// describe('GET /api/v1/gamemode ', () => {
+//   it.todo('Should get all game mode data', async () => {
+//     vi.mocked(db.getAllGameModes).mockResolvedValue(dummyDataArr)
+
+//     const res = await request(server).get(`${rootUrl}`)
+
+//     expect(res.statusCode).toBe(200)
+//     expect(res.body).toStrictEqual(dummyDataArr)
+//   })
+//   it('Should show an error message', async () => {
+//     vi.mocked(db.getAllGameModes).mockRejectedValue(dummyDataArr)
+
+//     const res = await request(server).get(`${rootUrl}`)
+
+//     expect(res.statusCode).toBe(500)
+//   })
+// })
