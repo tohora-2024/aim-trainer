4fd2c0046e1d044cf98d96a5080f76c3deb6bc31
Boston Phillips-Lepaio <bostonpl20@gmail.com>
2024-04-12 10:38:42 +1200

    rendering game-grid component test

--- client/components/__tests__/game-grid.test.tsx
+++ client/components/__tests__/game-grid.test.tsx
@@ -1,21 +1,23 @@
 //@vitest-environment jsdom
-import { describe, it, expect, beforeAll, afterEach, vi } from 'vitest'
+import { describe, it, expect, beforeAll } from 'vitest'
+import { MemoryRouter } from 'react-router-dom'
 import nock from 'nock'
-import { waitForElementToBeRemoved } from '@testing-library/react'
+import GameGrid from '../game-grid'
+import { render, screen } from '@testing-library/react'
 
 beforeAll(() => {
-  nock.disableNetConnect()
+  nock('http://example.com')
+    .get('/leaderboard')
+    .reply(200, { data: 'Leaderboard data' })
 })
 
-afterEach(() => {
-  vi.clearAllMocks()
-})
-
-const mockData = []
-
-describe('<Grid />', () => {
-  it('Should display an empty div', async () => {
-    // Arrange / Act
-    const scope = nock('http://localhost').get('/api/v1').reply(200, mockData)
+describe('GameGrid Component', () => {
+  it('renders the component correctly', async () => {
+    render(
+      <MemoryRouter>
+        <GameGrid onStartGame={() => {}} />
+      </MemoryRouter>,
+    )
+    expect(screen.getByText('1:00')).toBeTruthy()
   })
 })
