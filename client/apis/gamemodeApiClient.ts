a5cb5b1a311ffb788d5c205c4cab604efcc97da4
Jess Bayman <154103314+jess-bay@users.noreply.github.com>
2024-04-11 16:43:37 +1200

    Write gamemode api client functions

--- client/apis/gamemodeApiClient.ts
+++ client/apis/gamemodeApiClient.ts
@@ -3,12 +3,30 @@ import { GameMode, GameModeData } from '../../models/gamemode'
 
 const rootUrl = '/api/v1/gamemode'
 
-export async function getAllGameModes() {
+export async function getAllGameModes(): Promise<GameMode[]> {
   const res = await request.get(rootUrl)
   return res.body
 }
 
-export async function getGameModeById(id: number) {
+export async function getGameModeById(id: number): Promise<GameMode> {
   const res = await request.get(`${rootUrl}/${id}`)
+  return res.body
+}
+
+export async function addGameMode(gamemode: GameModeData) {
+  const res = await request.post(rootUrl).send(gamemode)
+  return res
+}
+
+export async function deleteGameModeById(id: number) {
+  const res = await request.delete(`${rootUrl}/${id}`)
+  return res.body
+}
+
+export async function updatedGameModeById(
+  id: number,
+  newGameMode: GameModeData,
+) {
+  const res = await request.put(`${rootUrl}/${id}`).send({ newGameMode })
   return res.body
 }
