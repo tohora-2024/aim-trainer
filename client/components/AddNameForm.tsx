6c2261eadd1493177ca457c01e47560959760f75
Jess Bayman <154103314+jess-bay@users.noreply.github.com>
2024-04-12 09:52:23 +1200

    Start on addNameForm

--- client/components/AddNameForm.tsx
+++ client/components/AddNameForm.tsx
@@ -1,3 +1,12 @@
+import { useState } from "react"
+import { useAddPlayer, useGetAllPlayers } from "../hooks/usePlayer"
+
 export default function AddNameForm() {
+  const {data} = useGetAllPlayers
+  const addMutation = useAddPlayer()
+  const [newName, setNewName] = useState('')
+
+  const handleName = (event: React.ChangeEvent<HTMLInputElement>) => 
+
   return <h1>Form</h1>
 }
