883868d392f55f945290f33829fb06db98b6c7cf
Boston Phillips-Lepaio <bostonpl20@gmail.com>
2024-04-12 13:39:45 +1200

    added more timer tests

--- client/components/__tests__/game-grid.test.tsx
+++ client/components/__tests__/game-grid.test.tsx
@@ -12,12 +12,28 @@ beforeAll(() => {
 })
 
 describe('GameGrid Component', () => {
-  it('renders the component correctly', async () => {
+  it('renders the timer for 1 minute long', async () => {
     render(
       <MemoryRouter>
-        <GameGrid onStartGame={() => {}} />
+        <GameGrid onStartGame={() => {}} duration={60000} />
       </MemoryRouter>,
     )
     expect(screen.getByText('1:00')).toBeTruthy()
+  })
+  it('renders the timer for 2 minutes long', async () => {
+    render(
+      <MemoryRouter>
+        <GameGrid onStartGame={() => {}} duration={120000} />
+      </MemoryRouter>,
+    )
+    expect(screen.getByText('2:00')).toBeTruthy()
+  })
+  it('renders the timer for 3 minutes long', async () => {
+    render(
+      <MemoryRouter>
+        <GameGrid onStartGame={() => {}} duration={180000} />
+      </MemoryRouter>,
+    )
+    expect(screen.getByText('3:00')).toBeTruthy()
   })
 })
