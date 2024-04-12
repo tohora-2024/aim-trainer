3ce859b36ca54002ce2d543292fa6090d67aa682
Joel Anderson <154105066+joel-anderson-24@users.noreply.github.com>
2024-04-12 15:22:17 +1200

    Add new data to player seeds

--- server/db/seeds/player.js
+++ server/db/seeds/player.js
@@ -5,5 +5,8 @@ export async function seed(knex) {
     { id: 1, name: 'Joel', score: 1, time_taken: '', gamemode_id: 1 },
     { id: 2, name: 'Jess', score: 10, time_taken: '', gamemode_id: 1 },
     { id: 3, name: 'Boston', score: 20, time_taken: '', gamemode_id: 1 },
+    { id: 4, name: 'Berhane', score: 10, time_taken: '', gamemode_id: 2 },
+    { id: 5, name: 'Gerard', score: 100, time_taken: '', gamemode_id: 2 },
+    { id: 6, name: 'Robert', score: 20, time_taken: '', gamemode_id: 3 },
   ])
 }
