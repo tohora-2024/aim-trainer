67a0be5176ad9dca85b35fde5aa3fa97818d0843
Jess Bayman <154103314+jess-bay@users.noreply.github.com>
2024-04-14 09:54:44 +1200

    Add skeleton for AddNameForm

--- client/components/AddNameForm.tsx
+++ client/components/AddNameForm.tsx
@@ -1,12 +1,38 @@
-// import { useState } from "react"
-// import { useAddPlayer, useGetAllPlayers } from "../hooks/usePlayer"
+import { useState } from 'react'
+import { useAddPlayer, useGetAllPlayers } from '../hooks/usePlayer'
 
-// export default function AddNameForm() {
-//   const {data} = useGetAllPlayers
-//   const addMutation = useAddPlayer()
-//   const [newName, setNewName] = useState('')
+export default function AddNameForm() {
+  const { data } = useGetAllPlayers()
+  const addMutation = useAddPlayer()
+  const [newName, setNewName] = useState('')
 
-//   const handleName = (event: React.ChangeEvent<HTMLInputElement>) =>
+  const handleName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewName(event.target.value)
+  }
 
-//   return <h1>Form</h1>
-// }
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const player = {
+      name: newName,
+    }
+    addMutation.mutate(player)
+    setNewName('')
+  }
+
+  return (
+    <>
+      <h3>Add Your Name</h3>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Enter Name</label>
+          <input
+            onChange={handleName}
+            placeholder="Name"
+            value={newName}
+            id="name"
+          />
+        </form>
+      </div>
+    </>
+  )
+}
