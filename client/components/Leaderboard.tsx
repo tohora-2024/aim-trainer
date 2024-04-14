16bf7a084d178945fda819bb8afef56dccedc9fa
Ben Atkin <63535606+ben-atkin@users.noreply.github.com>
2024-04-15 09:38:43 +1200

    Sort data to show highest score first

--- client/components/Leaderboard.tsx
+++ client/components/Leaderboard.tsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom'
 import { useGetGameModeAndPlayer } from '../hooks/useJoins'
 import AddNameForm from './AddNameForm'
-import HitCounter from './hit-counter'
+import { GameModeAndPlayerData } from '../../models/join'
 
 export default function Leaderboard() {
   const { gamemode } = useParams()
@@ -9,12 +9,21 @@ export default function Leaderboard() {
 
   console.log('data', data)
 
+  const data2 = data?.sort(function (
+    a: GameModeAndPlayerData,
+    b: GameModeAndPlayerData,
+  ) {
+    if (a.playerScore > b.playerScore) return -1
+    if (a.playerScore < b.playerScore) return 1
+    return 0
+  })
+
   return (
     <>
       <h1>LEADERBOARD</h1>
       {/* <AddNameForm score={hitCount} /> */}
       <div>
-        {data?.map((player) => (
+        {data2?.map((player) => (
           <div key={player?.playerId}>
             <p>Player: {player?.playerName}</p>
             <p>Score: {player?.playerScore}</p>
